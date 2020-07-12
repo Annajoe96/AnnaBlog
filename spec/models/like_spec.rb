0867b5a74cb2bb46a91ae4b8aa71fbf92932de09
Anna Joe <57370408+Annajoe96@users.noreply.github.com>
2020-07-12 21:16:48 +0100

    like model tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has a valid Factory' do
+    build(:like).expect be_valid
+  end
+
+  describe 'validations' do
+    it { expect validate_presence_of(:user) }
+    it { expect validate_presence_of(:article) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe User, :type => :model do
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:password) }
-    it { should validate_presence_of(:firstname) }
-    it { should validate_presence_of(:lastname) }
-    it { should validate_presence_of(:email) }
+    it { expect validate_presence_of(:password) }
+    it { expect validate_presence_of(:firstname) }
+    it { expect validate_presence_of(:lastname) }
+    it { expect validate_presence_of(:email) }
 
-    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { expect validate_uniqueness_of(:email).case_insensitive }
   end
 
   describe 'validations' do
