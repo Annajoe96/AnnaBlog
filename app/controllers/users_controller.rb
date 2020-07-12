38dc0aa14ca78c6772959324f49c8831d1d3816d
Anna Joe <57370408+Annajoe96@users.noreply.github.com>
2020-07-12 19:01:52 +0100

    showing likes on profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @articles = @user.articles.page(params[:user_page])
     @comments = @user.comments.page(params[:comment_page])
+    @likes = @user.likes.page(params[:like_page])
   end
 
 end
