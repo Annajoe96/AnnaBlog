1748fe5b46b661743e58dec87d1b29a0033ccc3d
Anna Joe <57370408+Annajoe96@users.noreply.github.com>
2020-07-22 23:21:39 +0100

    send grid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,3 +110,13 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'annablogger.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
